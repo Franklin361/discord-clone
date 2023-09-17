0a6b92415b865e461bd96f8ddeade2b92a2803f6
Franklin Martinez Lucas <frankomtzlucas361@gmail.com>
2023-09-17 09:13:45 -0600

    feat: scroll to bottom

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -4,11 +4,12 @@ import { format } from 'date-fns'
 import { Member, Message, Profile } from '@prisma/client';
 import { ChatWelcome } from './chat-welcome';
 import { useChatQuery } from '@/hooks/use-chat-query';
-import { Loader2, ServerCrash } from 'lucide-react';
-import { Fragment } from 'react';
+import { Loader, Loader2, ServerCrash } from 'lucide-react';
+import { ElementRef, Fragment, useRef } from 'react';
 import { ChatItem } from './chat-item';
 import { MessageWithMemberWithProfile } from '@/types';
 import { useChatSocket } from '@/hooks/use-chat-socket';
+import { useChatScroll } from '@/hooks/use-chat-scroll';
 
 interface Props {
   name: string
@@ -30,6 +31,9 @@ export const ChatMessages = (props: Props) => {
   const addKey = `chat:${props.chatId}:messages`
   const updateKey = `chat:${props.chatId}:messages:update`
 
+  const chatRef = useRef<ElementRef<'div'>>(null)
+  const bottomRef = useRef<ElementRef<'div'>>(null)
+
   const chatQuery = useChatQuery({
     apiUrl: props.apiUrl,
     paramKey: props.paramKey,
@@ -41,6 +45,14 @@ export const ChatMessages = (props: Props) => {
     addKey,
     updateKey,
     queryKey,
+  })
+
+  useChatScroll({
+    bottomRef,
+    chatRef,
+    loadMore: chatQuery.fetchNextPage,
+    shouldLoadMore: !chatQuery.isFetchingNextPage && !!chatQuery.hasNextPage,
+    count: chatQuery.data?.pages?.[0]?.items?.length ?? 0
   })
 
   if (chatQuery.status === 'loading') return (
@@ -58,13 +70,35 @@ export const ChatMessages = (props: Props) => {
   )
 
   return (
-    <div className='flex-1 flex flex-col py-4 overflow-y-auto'>
-      <div className='flex-1' />
-      <ChatWelcome
-        type={props.type}
-        name={props.name}
-      />
+    <div ref={chatRef} className='flex-1 flex flex-col py-4 overflow-y-auto' >
 
+      {!chatQuery.hasNextPage && <div className='flex-1' />}
+
+      {
+        !chatQuery.hasNextPage && (
+          <ChatWelcome
+            type={props.type}
+            name={props.name}
+          />
+        )
+      }
+
+      {
+        chatQuery.hasNextPage && (
+          <div className='flex  justify-center' >
+            {
+              chatQuery.isFetchingNextPage
+                ? <Loader className='h-6 w-6 text-zinc-500 animate-spin my-4' />
+                : <button
+                  className='text-zinc-500 dark:text-zinc-400 hover:text-zinc-600 dark:hover:text-zinc-300 transition my-4'
+                  onClick={() => chatQuery.fetchNextPage()}
+                >
+                  Load previous messages
+                </button>
+            }
+          </div>
+        )
+      }
       <div className='flex flex-col-reverse mt-auto '>
         {
           chatQuery.data?.pages.map((group, i) => (
@@ -90,7 +124,7 @@ export const ChatMessages = (props: Props) => {
           ))
         }
       </div>
-
+      <div ref={bottomRef} />
     </div>
   )
 }
